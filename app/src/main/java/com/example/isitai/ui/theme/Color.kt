0e5ef6971f54df75d178b6a938c677fd1bd5e769
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2026-02-13 21:19:05 -0800

    feat: add app theme and design system

--- app/src/main/java/com/example/isitai/ui/theme/Color.kt
+++ app/src/main/java/com/example/isitai/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.isitai.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Theme colors
+val PrimarySurface = Color(0xFFFFFFFF)
+val PrimaryText = Color(0xFF000000)
+val SecondaryText = Color(0xFF757575)
+val PlaceholderLight = Color(0xFFE0E0E0)
+val PlaceholderDark = Color(0xFFD9D9D9)
+val ButtonFill = Color(0xFF1A1A1A)
+val ButtonText = Color(0xFFFFFFFF)
+val BadgeBackground = Color(0xFFF5F5F5)
+val BadgeBorder = Color(0xFF212121)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Annotation colors (not part of M3 scheme)
+object AnnotationColors {
+    val Anatomical = Color(0xFF1565C0)
+    val Texture = Color(0xFFC62828)
+    val Background = Color(0xFF6A1B9A)
+    val Facial = Color(0xFFF57F17)
+    val Lighting = Color(0xFF00695C)
+}
