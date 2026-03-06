5dcb7c277824033df4b0cf680b786ec0710b3035
Kevin Rutledge <kevin.rutledge.89@gmail.com>
2026-03-06 12:25:26 -0800

    feat: add pack API endpoints

--- app/src/main/java/com/example/isitai/data/remote/ContentApiService.kt
+++ app/src/main/java/com/example/isitai/data/remote/ContentApiService.kt
@@ -1,9 +1,17 @@
 package com.example.isitai.data.remote
 
 import com.example.isitai.data.model.ContentItem
+import com.example.isitai.data.model.PackMetadata
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ContentApiService {
     @GET("content.json")
     suspend fun getContentManifest(): List<ContentItem>
+
+    @GET("packs.json")
+    suspend fun getPackManifest(): List<PackMetadata>
+
+    @GET("packs/{packId}/manifest.json")
+    suspend fun getPackContent(@Path("packId") packId: String): List<ContentItem>
 }
